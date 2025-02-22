6a868833acbaf509daa590ae9567ba216af8fd6f
SergioBejarano <157920809+SergioBejarano@users.noreply.github.com>
2025-02-22 12:36:50 -0500

    Update 3.sh

--- 3.sh
+++ 3.sh
@@ -2,7 +2,7 @@
 #traversing the file system from a given directory, including subdirectories
 
 if [ $# -ne 3 ]; then
-	echo "Usage <directory> <no_files> <max_size>"
+	echo "Usage <no_files> <max_size>"
 	exit 1;
 fi
 
@@ -12,7 +12,4 @@ size=$3
 
 # shows the n smallest files within a size specified by the user.
 echo -e "Files founds:\n"
-find "$direct" -type f -size "-${size}c" 2>/dev/null | head -n "$number" | while read file; do
-    # Using stat to get the size and name of the file
-    stat --format="%s %n" "$file"
-done
+find "$direct" -type f -size "-${size}c" 2>/dev/null -exec ls -l {} + | sort -k 5 -n | head "$number" | awk '{print $5, $9}'
